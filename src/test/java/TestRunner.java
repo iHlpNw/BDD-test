ecc6012d4cb7b5c43072abd7de98d455a46c72b4
xHlpNw <aleksashin990@gmail.com>
2024-11-18 11:59:19 +0400

    Corrected the annotations

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,16 +1,17 @@
-import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.platform.engine.Cucumber;
 import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.runner.RunWith;
 
-@Suite
-@IncludeEngines("cucumber")
-@RunWith(Cucumber.class)
-@CucumberOptions(
-        features = "features",  // Путь к .feature файлам
-        glue= {"stepDefinition"},
-        plugin = {"pretty", "html:target/cucumber-reports"}  // Плагины для отчетности
-)
+//@Suite
+//@IncludeEngines("cucumber")
+//@RunWith(Cucumber.class)
+//@CucumberOptions(
+//        features = "features",  // Путь к .feature файлам
+//        glue= {"stepDefinition"},
+//        plugin = {"pretty", "html:target/cucumber-reports"}  // Плагины для отчетности
+//)
+@Cucumber
 public class TestRunner {
 }
